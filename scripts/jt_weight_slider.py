ab6297cee64c47f6533747e1e4d05f01d96607dc
Jonathan Topf <jonathan@jonathantopf.com>
2013-04-08 18:32:00 +0100

    fixed a tab - spaces formatting error

--- scripts/jt_weight_slider.py
+++ scripts/jt_weight_slider.py
@@ -37,14 +37,14 @@ def load_ui():
 def get_bone(jointnumb):
 
     selected_joints = cmds.ls(sl=True, typ='joint')
-	if len(selected_joints) > 1:
-		jointtext = ["joint_disp_" + str(jointnumb), "joint_disp_" + str(1 - jointnumb)]
-		cmds.textField(jointtext[0], e=True, tx=selected_joints[0])
-		cmds.textField(jointtext[1], e=True, tx=selected_joints[1])
-	else:
-		jointtext = "joint_disp_" + str(jointnumb)
-		cmds.textField(jointtext, e=True, tx=selected_joints[0])
-	
+    if len(selected_joints) > 1:
+        jointtext = ["joint_disp_" + str(jointnumb), "joint_disp_" + str(1 - jointnumb)]
+        cmds.textField(jointtext[0], e=True, tx=selected_joints[0])
+        cmds.textField(jointtext[1], e=True, tx=selected_joints[1])
+    else:
+        jointtext = "joint_disp_" + str(jointnumb)
+        cmds.textField(jointtext, e=True, tx=selected_joints[0])
+
 
 def get_skinCluster(joints):
 
@@ -67,14 +67,12 @@ def set_weights():
     skincluster = get_skinCluster(stored_joints[0])
     
     if len(stored_joints) == 1:
-    	cmds.skinPercent(skincluster, tv=(stored_joints[0], 1 - slider_value))
-    	cmds.skinPercent(skincluster, pruneWeights=0.01 )
-    	
+        cmds.skinPercent(skincluster, tv=(stored_joints[0], 1 - slider_value))
+        cmds.skinPercent(skincluster, pruneWeights=0.01 )
+        
     else:
-    	cmds.skinPercent(skincluster, tv=[(stored_joints[0], 1 - slider_value),(stored_joints[1], slider_value)])
-    	cmds.skinPercent(skincluster, pruneWeights=0.01 )
-    #print(slider_value)
-    #print(inv_slider_value)
+        cmds.skinPercent(skincluster, tv=[(stored_joints[0], 1 - slider_value),(stored_joints[1], slider_value)])
+        cmds.skinPercent(skincluster, pruneWeights=0.01 )
 
 
 def modify_weights(num):
