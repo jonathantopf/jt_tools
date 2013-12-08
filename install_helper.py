1cecdb2cafac799e62ab63b5098d8959c54c0a47
Jonathan Topf <jonathan@jonathantopf.com>
2013-12-08 00:54:44 +0000

    updated installation to module system

--- install_helper.py
+++ install_helper.py
@@ -1,6 +1,6 @@
 
 #
-# Copyright (c) 2013 Jonathan Topf
+# Copyright (c) 2012-2013 Jonathan Topf
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,40 +21,62 @@
 # THE SOFTWARE.
 #
 
-
-import sys
-import os
 import maya.cmds as cmds
+import maya.mel as mel
+import os
 import sys
-import inspect
 
-def install(module_dir, install_dir):
+def install():
 
-    installation_name = 'jt_tools'
+    cmds.confirmDialog(m='After clicking \'OK\' a file dialogue will open. Please navigate to and select the jt_tools directory and click \'select\'.', button=['ok'])
 
-    if not os.path.exists(module_dir):
-        os.mkdir(module_dir)
+    maya_app_dir = mel.eval('getenv MAYA_APP_DIR')
+    jt_tools_dir = cmds.fileDialog2(fm=2, okc='Select', cap='Select jt_tools directory')
+    
+    if jt_tools_dir is not None:
+        
+        jt_tools_dir = jt_tools_dir[0]
+        
+        try:
+            sys.path.append(os.path.join(jt_tools_dir, 'scripts'))
+            import ms_commands
+        except:
+            print 'No valid jt_tools directory found.'
+            return False
+        
+        
+        modules_path = os.path.join(maya_app_dir, 'modules')
+        
+        if not os.path.exists(modules_path):
+            print '{0} does not exist, creating...'.format(modules_path)
+            os.makedirs(modules_path)
+            
+            if not os.path.exists(modules_path):
+                print 'Failed to create module directory.'
+                return False
+                    
+        module_file_path = os.path.join(modules_path, 'jt_tools.module')
+        
+        if os.path.exists(module_file_path):
+            continue_return_value = cmds.confirmDialog(m='A jt_tools module file already exists from a previous instalation, would you like to overwrite it?', button=['yes','no'])
+            if continue_return_value == 'no':
+                return False
+        
+        try:
+            module_file_contents = "+ jt_tools {0} {1} \n\nicons: graphics \nscripts: scripts".format('1.0', jt_tools_dir)
+            file = open(module_file_path, 'w')
+            file.write(module_file_contents)
+            file.close()
+            
+        except:
+            print 'Error creating the jt_tools module file'
+            return False
+                
+    return True
 
-    file = open(os.path.join(module_dir, installation_name + '.mod'), 'w')
+def install_jt_tools(): 
 
-    module_definition = """
-
-+ jt_tools 0.0.1 {0}
-
-icons: icons 
-scripts: sctpts 
-#presets: presets """.format(install_dir)
-
-    file.write(module_definition)
-
-    file.close()
-
-
-
-    current_script_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    sys.path.append(os.path.join(current_script_path, 'scripts'))
-    import jt_tools    
-    jt_tools.create_shelf('jt_tools')
-    sys.path.remove(os.path.join(current_script_path, 'scripts'))
-
-    cmds.confirmDialog(title=installation_name + ' install', message='All done!, just restart and enable and plugins not already enabled in the plugin manager. jt', button='OK')
+    if not install():
+        cmds.confirmDialog(m='jt_tools was not installed. See the script editor for details.', button=['ok'])
+    else:
+        cmds.confirmDialog(m='jt_tools has been successfully installed. Please restart maya and enable jt_tools.py from the plugin manager.', button=['ok'])
